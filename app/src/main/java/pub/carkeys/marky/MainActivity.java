865379cd46717425833245050433880aa874880f
James Keesey <keesey@carkees.net>
2018-05-24 16:20:33 -0500

    Sign in extracted to separate helper class.

--- app/src/main/java/pub/carkeys/marky/MainActivity.java
+++ app/src/main/java/pub/carkeys/marky/MainActivity.java
@@ -2,28 +2,26 @@ package pub.carkeys.marky;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
 
-import com.firebase.ui.auth.AuthUI;
-import com.firebase.ui.auth.FirebaseUiException;
-import com.firebase.ui.auth.IdpResponse;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity {
-  private static final int RC_SIGN_IN = 5647;
+public class MainActivity extends AppCompatActivity implements SignInHandler.SignInCallback {
+  private SignInHandler signInHandler;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
+    signInHandler = new SignInHandler(this);
 
     setContentView(R.layout.activity_main);
     Toolbar toolbar = findViewById(R.id.toolbar);
@@ -35,51 +33,20 @@ public class MainActivity extends AppCompatActivity {
       public void onClick(View view) {
         FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
         if (currentUser == null) {
-          signIn();
+          signInHandler.signIn();
         } else {
-          signOut();
+          signInHandler.signOut();
         }
       }
     });
-  }
-
-  private void signIn() {
-    // Choose authentication providers
-    List<AuthUI.IdpConfig> providers = Arrays.asList(new AuthUI.IdpConfig.GoogleBuilder().build(),
-        new AuthUI.IdpConfig.TwitterBuilder().build(), new AuthUI.IdpConfig.EmailBuilder().build());
-    // Create and launch sign-in intent
-    startActivityForResult(
-        AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(providers).build(),
-        RC_SIGN_IN);
-  }
-
-  private void signOut() {
-    
   }
 
   @Override
   protected void onActivityResult(int requestCode, int resultCode, Intent data) {
     super.onActivityResult(requestCode, resultCode, data);
 
-    if (requestCode == RC_SIGN_IN) {
-      IdpResponse response = IdpResponse.fromResultIntent(data);
-
-      if (resultCode == RESULT_OK) {
-        // Successfully signed in
-        showUserName();
-      } else {
-        if (response == null) {
-          showMessage("Login Canceled");
-        } else {
-          final FirebaseUiException error = Objects.requireNonNull(response.getError());
-          final String message = error.getMessage();
-          showMessage(message);
-        }
-        // Sign in failed. If response is null the user canceled the
-        // sign-in flow using the back button. Otherwise check
-        // response.getError().getErrorCode() and handle the error.
-        // ...
-      }
+    if (requestCode == SignInHandler.RC_SIGN_IN) {
+      signInHandler.handleActivityResult(requestCode, resultCode, data);
     }
   }
 
@@ -90,16 +57,44 @@ public class MainActivity extends AppCompatActivity {
   }
 
   private void showUserName() {
+    showMessage(String.format("who: %s", getUserName()));
+  }
+
+  @NonNull
+  private String getUserName() {
     FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
     //updateUI(currentUser);
     String userName = "<not logged in>";
     if (currentUser != null) {
       userName = currentUser.getDisplayName();
+      if (userName == null) {
+        userName = "<unknown>";
+      }
     }
-    showMessage("who: " + userName);
+    return userName;
   }
 
   private void showMessage(final String message) {
-    (new AlertDialog.Builder(this)).setMessage(message).create().show();
+    Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+  }
+
+  @Override
+  public void onSignedIn() {
+    showMessage(String.format("Signed in as %s.", getUserName()));
+  }
+
+  @Override
+  public void onSignedOut() {
+    showMessage("Signed out.");
+  }
+
+  @Override
+  public void onSignInCanceled() {
+    showMessage("Sign in cancelled.");
+  }
+
+  @Override
+  public void onSignInFailed(final int errorCode, final String message) {
+    showMessage(String.format(Locale.US, "Sign in failure (%d): %s.", errorCode, message));
   }
 }
---
+++ app/src/main/java/pub/carkeys/marky/SignInHandler.java
@@ -0,0 +1,96 @@
+package pub.carkeys.marky;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.FirebaseUiException;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
+import static android.app.Activity.RESULT_OK;
+
+public final class SignInHandler {
+  public static final int RC_SIGN_IN = 5647;
+
+  private final Activity activity;
+  private final List<AuthUI.IdpConfig> providers = new LinkedList<>();
+
+  public SignInHandler(final Activity activity) {
+    this(activity,
+         Arrays.asList(new AuthUI.IdpConfig.GoogleBuilder().build(),
+                       new AuthUI.IdpConfig.TwitterBuilder().build(),
+                       new AuthUI.IdpConfig.EmailBuilder().build()));
+  }
+
+  @SuppressWarnings("WeakerAccess")
+  public SignInHandler(final Activity activity,
+                       @NonNull Collection<? extends AuthUI.IdpConfig> providers) {
+    this.activity = activity;
+    this.providers.addAll(providers);
+  }
+
+  public void signIn() {
+    activity.startActivityForResult(AuthUI.getInstance()
+                                          .createSignInIntentBuilder()
+                                          .setAvailableProviders(providers)
+                                          .build(), RC_SIGN_IN);
+  }
+
+  public void signOut() {
+    AuthUI.getInstance().signOut(activity).addOnCompleteListener(new OnCompleteListener<Void>() {
+      public void onComplete(@NonNull Task<Void> task) {
+        if (activity instanceof SignInCallback) {
+          ((SignInCallback) activity).onSignedOut();
+        }
+      }
+    });
+  }
+
+  public void handleActivityResult(int requestCode, int resultCode, Intent data) {
+    if (requestCode == RC_SIGN_IN) {
+      IdpResponse response = IdpResponse.fromResultIntent(data);
+
+      if (resultCode == RESULT_OK) {
+        if (activity instanceof SignInCallback) {
+          ((SignInCallback) activity).onSignedIn();
+        }
+      } else {
+        if (response == null) {
+          if (activity instanceof SignInCallback) {
+            ((SignInCallback) activity).onSignInCanceled();
+          }
+        } else {
+          if (activity instanceof SignInCallback) {
+            int errorCode = ErrorCodes.UNKNOWN_ERROR;
+            String message = "Unknown error";
+            final FirebaseUiException error = response.getError();
+            if (error != null) {
+              errorCode = error.getErrorCode();
+              message = error.getLocalizedMessage();
+            }
+            ((SignInCallback) activity).onSignInFailed(errorCode, message);
+          }
+        }
+      }
+    }
+  }
+
+  public interface SignInCallback {
+    void onSignedIn();
+
+    void onSignedOut();
+
+    void onSignInCanceled();
+
+    void onSignInFailed(int errorCode, String message);
+  }
+}
